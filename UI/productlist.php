598807e3fc338f11bb250c093696a6546763286e
Vattanac <vattanac.kong.stm@gmail.com>
2025-06-13 22:39:27 +0700

    View Product Done and Change Composer and Clone php_barcode manually to the vendor folder

---
+++ UI/productlist.php
@@ -0,0 +1,117 @@
+<?php
+include_once ('connectdb.php');
+session_start();
+if ($_SESSION['email']=="" OR $_SESSION['role']=="user") {
+  header('location: ../index.php');
+}
+
+
+include('header.php');
+
+?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Product List</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 ">
+
+          <div class="card card-primary card-outline">
+            <div class="card-header">
+              <h5 class="m-0">Product List</h5>
+            </div>
+            <div class="card-body">
+              <table class="table table-striped" id="product_table">
+                <thead>
+                <tr>
+                  <td>Barcode</td>
+                  <td>Product</td>
+                  <td>Category</td>
+                  <td>Description</td>
+                  <td>Stock</td>
+                  <td>Purchase Price</td>
+                  <td>Sale Price</td>
+                  <td>Image</td>
+                  <td>Actions</td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $select = $pdo->prepare("SELECT * FROM tbl_product order by product_id asc ");
+                $select->execute();
+
+                while($row=$select->fetch(PDO::FETCH_OBJ)){
+                  echo '
+                         <tr xmlns="http://www.w3.org/1999/html">
+                           <td>' .$row->barcode.'</td>
+                           <td>'.$row->product.'</td>
+                           <td>'.$row->category.'</td>
+                           <td>'.$row->description.'</td>
+                           <td>'.$row->stock.'</td>
+                           <td>'.$row->purchase_price.'</td>
+                           <td>'.$row->sale_price.'</td>
+                           <td><img src="../'.$row->image_path.'" class="img-rounded" width="60px" height="60px"> </td>
+                           <td>
+                            <div class="btn-group">
+                                <a href="printbarcode.php?id='.$row->product_id.'" class="btn btn-primary btn-xs" role="button">
+                                    <span class="fa fa-barcode" style="color: #FFFFFF" data-toggle="tooltip" title="Print Barcode" "></span>
+                                </a>
+                                <a href="viewproduct.php?id='.$row->product_id.'" class="btn btn-warning btn-xs" role="button">
+                                    <span class="fa fa-eye" style="color: #FFFFFF" data-toggle="tooltip" title="View Product" "></span>
+                                </a>
+                                <a href="editproduct.php?id='.$row->product_id.'" class="btn btn-success btn-xs" role="button">
+                                    <span class="fa fa-edit" style="color: #FFFFFF" data-toggle="tooltip" title="Edit Product" "></span>
+                                </a>
+                                <button id="'.$row->product_id.'" class="btn btn-danger btn-xs">
+                                    <span class="fa fa-trash" style="color: #FFFFFF" data-toggle="tooltip" title="Delete Product" "></span>
+                                </button>
+                            </div>
+                           </td>
+                         </tr>
+                        ';
+                }
+                ?>
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-6 -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include('footer.php');
+?>
+
+<script>
+  $(document).ready(function (){
+    $('#product_table').DataTable();
+  });
+
+  $(document).ready(function (){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+</script>
+
---
+++ UI/viewproduct.php
@@ -0,0 +1,88 @@
+<?php
+include_once ('connectdb.php');
+require __DIR__ . '/../vendor/php_barcode/barcode128.php';
+
+session_start();
+if ($_SESSION['email']=="" OR $_SESSION['role']=="user") {
+  header('location: ../index.php');
+}
+
+
+include('header.php');
+
+?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 ">
+
+          <div class="card card-primary card-outline">
+            <div class="card-header">
+              <h5 class="m-0">View Product</h5>
+            </div>
+            <div class="card-body">
+              <?php
+              $id = $_GET['id'];
+
+              $select = $pdo->prepare("SELECT * FROM tbl_product WHERE product_id=$id");
+              $select->execute();
+
+              while ($row = $select->fetch(PDO::FETCH_OBJ)) {
+
+                echo '
+                  <div class="row">
+                    <div class="col-md-6">
+                      <p class="list-group-item list-group-item-info"><b>Product Details</b></p>
+                      <ul class="list-group">
+                        <li class="list-group-item"><b>Barcode</b> <span class="badge badge-light float-right">'.bar128($row->barcode).'</span></li>
+                        <li class="list-group-item"><b>Product</b> <span class="badge badge-warning float-right">'. $row->product.'</span></li>
+                        <li class="list-group-item"><b>Category</b> <span class="badge  badge-success float-right">'.$row->category.'</span></li>
+                        <li class="list-group-item"><b>Description </b><span class="badge badge-primary float-right">'. $row->description.'</span></li>
+                        <li class="list-group-item"><b>Stock </b><span class="badge badge-danger float-right">'. $row->stock .'</span></li>
+                        <li class="list-group-item"><b>Purchase Price</b> <span class="badge badge-secondary float-right">'.$row->purchase_price.'</span></li>
+                        <li class="list-group-item"><b>Sale Price </b> <span class="badge badge-dark float-right">'.$row->sale_price.'</span></li>
+                        <li class="list-group-item"><b>Product Profit </b> <span class="badge badge-success float-right">'.($row->sale_price - $row->purchase_price).'</span></li>
+
+                      </ul>
+                    </div>
+                    <div class="col-md-6">
+                      <p class="list-group-item list-group-item-info"  ><b>Product Image</b></p>
+                      <img src="../'.$row->image_path.'" class="img img-thumbnail img-responsive">
+                    </div>
+                  </div>';
+
+              }
+
+              ?>
+
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-6 -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include('footer.php');
+?>
